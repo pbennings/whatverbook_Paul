aa73a8eaf1f1979f549607e00e6463d003c1aa82
Colin Jaffe <colin.jaffe@gmail.com>
2019-03-26 15:08:50 -0400

    removes almost all code

---
+++ README.md
@@ -0,0 +1,3 @@
+# whateverbook-solution
+
+A possible solution to [the whateverbook project](https://github.com/abbreviatedman/whateverbook).
---
+++ data.js
@@ -0,0 +1,61 @@
+const instructors = [
+    {
+        firstName: 'Colin',
+        lastName: 'Jaffe',
+        term: 1,
+        usesLinux: true,
+        position: `Head Instructor`,
+        topicsCovered: [
+            'Responsive Design',
+            'JavaScript',
+            'Bash',
+            'Git'
+        ]
+    },
+    {
+        firstName: 'Yuri',
+        lastName: 'Shkoda',
+        term: 2,
+        usesLinux: false,
+        position: `Head Instructor`,
+        topicsCovered: [
+            'Databases',
+            'Routing',
+            'Beginning React',
+            'Algorithms and Data Structures'
+        ]
+    },
+    {
+        firstName: 'Pak',
+        lastName: 'Chu',
+        term: 3,
+        usesLinux: false,
+        position: 'Head Instructor',
+        topicsCovered: [
+            'Advanced React',
+            'Interviewing',
+            'Putting It All Together'
+        ]
+    },
+    {
+        firstName: 'Ginny',
+        lastName: 'Gu Tang',
+        term: 1,
+        usesLinux: false,
+        position: `Teacher's Assistant`
+    },
+    {
+        firstName: 'Sonny',
+        lastName: 'Rahman',
+        term: 2,
+        usesLinux: false,
+        position: `Teacher's Assistant`
+    },
+    {
+        firstName: `Michael`,
+        lastName: `Dwyer`,
+        term: 3,
+        usesLinux: false,
+        position: `Teacher's Assistant`
+    }
+];
